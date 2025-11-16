babef93dffaff56a5b9fa0efa1099e4b7957c58a
Yousef Al-Salqan <24254444@studentmail.ul.ie>
2025-11-16 13:35:47 +0000

    CLI Interface Code
    I have created the CLI interface. I need the student, lecturer, and admin classes for this to run.
    
    I will note with comments later where we will change once we have the CSV.

--- src/main/java/ui/cli/AdminCommandSession.java
+++ src/main/java/ui/cli/AdminCommandSession.java
@@ -100,7 +100,6 @@ public class AdminCommandSession implements UserSession {
     private void cmdUpdateStudent(String[] args) {
         if (args.length < 3) {
             System.out.println("Usage: update-student <id> <field> <newValue>");
-            System.out.println("Fields: student_name");
             return;
         }
         String id = args[0];
