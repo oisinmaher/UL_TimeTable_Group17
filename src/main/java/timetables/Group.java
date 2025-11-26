97e262d2101c87c5de11c50cbb1ad2113b998d44
oisinmaher <oisinmaher527@gmail.com>
2025-11-26 16:14:56 +0000

    When timeslots are created they get added to a course map
    (cherry picked from commit a87d13a67032e85466ed17cbf7f11dcf61a7cb3c)

--- src/main/java/timetables/Group.java
+++ src/main/java/timetables/Group.java
@@ -16,67 +16,42 @@ public class Group {
     protected final CourseModule module;
     protected final String moduleCode;
     protected final Map<String, Student> studentsInGroup;
-    private Teacher teacher;
-    private final List<Room> rooms;
-    private final Map<String, SubGroup> referencedSubGroups;
-    private final Set<String> usedTimes;
-    private Map<String, TimeSlot> lecturesTimes;
-    private Map<String, TimeSlot> labsTimes;
-    private Map<String, TimeSlot> tutorialsTimes;
+    protected Teacher lecturer;
+    private final Map<String, Room> rooms;
+    private final Map<String, TimeSlot> referencedTimeSlots;
+    protected Set<String> usedTimes;
+    protected Map<String, TimeSlot> lecturesTimes;
+    protected Map<String, TimeSlot> labsTimes;
+    protected Map<String, TimeSlot> tutorialsTimes;
+    protected CourseFull courseFull;
 
-    public Group(String moduleCode){
-        if (moduleCode == null) {
+    public Group(CourseModule courseModule, CourseFull courseFull){
+        if (courseModule == null) {
             throw new IllegalArgumentException("Module can't be null");
         }
-        moduleCode = moduleCode.toLowerCase();
-        this.moduleCode = moduleCode;
-        this.module = CourseModule.getModuleFromCode(moduleCode);
-        this.teacher = null;
-        rooms = new ArrayList<>();
-        referencedSubGroups = new HashMap<>();
+        this.module = courseModule;
+        this.moduleCode = courseModule.getModuleCode().toLowerCase();
+        this.lecturer = null;
+        rooms = new HashMap<>();
+        referencedTimeSlots = new HashMap<>();
         studentsInGroup = new HashMap<>();
         this.lecturesTimes = new HashMap<>();
         this.labsTimes = new HashMap<>();
         this.tutorialsTimes = new HashMap<>();
         this.usedTimes = new HashSet<>();
+        this.courseFull = courseFull;
     }
 
-    /**
-     * Allows creation of timeslots for this group/module
-     *
-     * @param typeOfClass
-     * @param day
-     * @param time
-     */
-    public void addTimeSlot(String typeOfClass, String day, String time){
-        typeOfClass = typeOfClass.toLowerCase();
-        if(typeOfClass.equals("lecture")){
-            TimeSlot timeSlot = new TimeSlot(day, time, module);
-            if(usedTimes.contains(timeSlot.toString())){
-                throw new IllegalArgumentException("This timeslot is already used");
-            }
-            usedTimes.add(timeSlot.toString());
-            lecturesTimes.put(timeSlot.toString(), timeSlot);
+    public void addLectureTime(String day, String time, String roomId, String teacherId){
+        TimeSlot timeSlot = new TimeSlot(day, time, module, courseFull, this, "Lecture", roomId, teacherId);
+        if(usedTimes.contains(timeSlot.getTime())){
+           throw new IllegalArgumentException("This timeslot is already taken");
         }
-        else if(typeOfClass.equals("lab")){
-            TimeSlot timeSlot = new TimeSlot(day, time, module);
-            if(usedTimes.contains(timeSlot.toString())){
-                throw new IllegalArgumentException("This timeslot is already used");
-            }
-            usedTimes.add(timeSlot.toString());
-            labsTimes.put(timeSlot.toString(), timeSlot);
-        }
-        else if(typeOfClass.equals("tutorial")){
-            TimeSlot timeSlot = new TimeSlot(day, time, module);
-            if(usedTimes.contains(timeSlot.toString())){
-                throw new IllegalArgumentException("This timeslot is already used");
-            }
-            usedTimes.add(timeSlot.toString());
-            tutorialsTimes.put(timeSlot.toString(), timeSlot);
-        }
-        else{
-            throw new IllegalArgumentException("This type of class doesnt exist, \n please chose lecture, lah or tutorial");
-        }
+        int prevLecHours = module.getLecHours();
+        module.setLecHours(prevLecHours + 1);
+        usedTimes.add(timeSlot.getTime());
+        lecturesTimes.put(timeSlot.getTime(), timeSlot);
+        referencedTimeSlots.put(timeSlot.getTime(), timeSlot);
     }
     public List<TimeSlot> getTimeSlots(String typeOfClass){
         typeOfClass = typeOfClass.toLowerCase();
@@ -93,10 +68,40 @@ public class Group {
             throw new IllegalArgumentException(typeOfClass + " isn't a type of class \nPlease choose lecture, lab, or tutorial");
         }
     }
-    public void createSubGroup(String typeOfClass){
+    /**
+     * Allows creation of timeslots in labs/tutorials for this group/module
+     *
+     * @param typeOfClass
+     * @param day
+     * @param time
+     */
+    public void addClassTimes(String typeOfClass, String day, String time, String roomId, String teacherId){
         typeOfClass = typeOfClass.toLowerCase();
-        SubGroup subGroup = new SubGroup(moduleCode, typeOfClass);
-        referencedSubGroups.put(subGroup.getSubGroupId(), subGroup);
+        if(typeOfClass.equals("lab")){
+            TimeSlot timeSlot = new TimeSlot(day, time, module, courseFull, this, "Lab", roomId, teacherId);
+            if(usedTimes.contains(timeSlot.getTime())){
+                throw new IllegalArgumentException("This timeslot is already used");
+            }
+            int prevLabHours = module.getLabHours();
+            module.setLabHours(prevLabHours+1);
+            usedTimes.add(timeSlot.getTime());
+            labsTimes.put(timeSlot.getTime(), timeSlot);
+            referencedTimeSlots.put(timeSlot.getTime(), timeSlot);
+        }
+        else if(typeOfClass.equals("tutorial") || typeOfClass.equals("tut")){
+            TimeSlot timeSlot = new TimeSlot(day, time, module, courseFull, this, "Tutorial", roomId, teacherId);
+            if(usedTimes.contains(timeSlot.getTime())){
+                throw new IllegalArgumentException("This timeslot is already used");
+            }
+            int prevTutHours = module.getTutHours();
+            module.setTutHours(prevTutHours + 1);
+            usedTimes.add(timeSlot.getTime());
+            tutorialsTimes.put(timeSlot.getTime(), timeSlot);
+            referencedTimeSlots.put(timeSlot.getTime(), timeSlot);
+        }
+        else{
+            throw new IllegalArgumentException("This type of class doesnt exist, \n please chose lab or tutorial");
+        }
     }
     public void addStudent(String studentId){
         studentsInGroup.put(studentId, Student.getStudentFromId(studentId));
@@ -112,14 +117,14 @@ public class Group {
 
 
     public void setTeacher(String teacherId){
-        this.teacher = Teacher.getTeacherFromId(teacherId);
+        this.lecturer = Teacher.getTeacherFromId(teacherId);
     }
     public Teacher getTeacherObject() {
-        return teacher;
+        return lecturer;
     }
     public String getTeacherName(){
-        if(this.teacher == null) return "Teacher is yet to be assigned";
-        return this.teacher.getName();
+        if(this.lecturer == null) return "Teacher is yet to be assigned";
+        return this.lecturer.getName();
     }
 
     public List<Student> getStudentsObject() {
@@ -133,18 +138,15 @@ public class Group {
         return studentsInGroup.size();
     }
 
-    public String getModuleCode() {
-        return module.getModuleCode();
-    }
 
     public List<Room> getRooms()
     {
-        return this.rooms; 
+        return new ArrayList<>(this.rooms.values());
     }
 
-    public String getGroupModuleCode()
-    {
-        return this.module.getModuleCode();
+
+    public CourseModule getCourseModule(){
+        return this.module;
     }
 
     @Override
@@ -164,8 +166,8 @@ public class Group {
         }
 
         return "Group{" +
-                ", module=" + module.getModuleCode() +
-                ", presiding teacher=" + this.teacher.getUserId() +
+                ", module=" + moduleCode +
+                ", presiding teacher=" + this.lecturer.getUserId() +
                 ", students=[" + sb +
                 "], rooms=" + rooms +
                 '}';
