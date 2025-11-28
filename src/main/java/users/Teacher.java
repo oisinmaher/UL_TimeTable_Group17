136b41646f1a358ba8ad9f75ac955436b6e6f025
oisinmaher <62963896+oisinmaher@users.noreply.github.com>
2025-11-28 22:39:39 +0000

    Update CourseTaught to ModulesTaught

--- src/main/java/users/Teacher.java
+++ src/main/java/users/Teacher.java
@@ -12,31 +12,30 @@ public class Teacher extends User {
 
     private final static Map<String, Teacher> allTeachers = new HashMap<>();
     // List of courses the teacher teaches (e.g LM121, LM051 etc.)
-     private final Map<String, CourseModule> coursesTaught;
-
+     private final Map<String, CourseModule> modulesTaught;
     // Constructor with base parameters of user
     public Teacher(String teacherId, String name) {
         super(teacherId, name);
-        coursesTaught = new HashMap<>();
+        modulesTaught = new HashMap<>();
         allTeachers.put(this.userId, this);
     }
 
     public void addCourseToTeacher(String courseCode){
         courseCode = courseCode.toLowerCase();
         CourseModule courseModule = CourseModule.getModuleFromCode(courseCode);
-        coursesTaught.put(courseCode, courseModule);
+        modulesTaught.put(courseCode, courseModule);
     }
     public void removeCourseFromTeacher(String courseCode){
         courseCode = courseCode.toLowerCase();
         if(containsCourse(courseCode)){
-            coursesTaught.remove(courseCode);
+            modulesTaught.remove(courseCode);
         }
         else{
             throw new IllegalArgumentException("Teacher doesnt teach this course");
         }
     }
     private boolean containsCourse(String courseCode){
-        return coursesTaught.containsKey(courseCode);
+        return modulesTaught.containsKey(courseCode);
     }
     private static boolean checkTeacherId(String teacherId) {
         return allTeachers.containsKey(teacherId);
